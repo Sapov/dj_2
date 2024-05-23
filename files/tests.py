758fc1efdd7cf3fe026340fc18cbc306f7be09d9
Sapov <Sapov@mail.ru>
2024-05-23 19:41:19 +0300

    add test

--- files/tests.py
+++ files/tests.py
@@ -4,20 +4,20 @@ from django.test import TestCase
 
 from .models import StatusProduct
 
+
 class StatusProductTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        #Set up non-modified objects used by all test methods
+        # Set up non-modified objects used by all test methods
         StatusProduct.objects.create(status='Оформлен')
 
     def test_status_label(self):
-        status=StatusProduct.objects.get(id=1)
+        status = StatusProduct.objects.get(id=1)
         field_label = status._meta.get_field('status').verbose_name
-        self.assertEquals(field_label,'Статус файла')
-
+        self.assertEquals(field_label, 'Статус файла')
 
     def test_status_length(self):
-        status=StatusProduct.objects.get(id=1)
+        status = StatusProduct.objects.get(id=1)
         max_length = status._meta.get_field('status').max_length
-        self.assertEquals(max_length,64)
+        self.assertEquals(max_length, 64)
